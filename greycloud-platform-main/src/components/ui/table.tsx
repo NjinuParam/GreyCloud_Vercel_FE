9b324d9e4437a67801e271977c212f6f90a08fb2
NjinuParam <njinu@webparam.org>
2024-09-19 15:19:29 +0200

    feat(up)

--- greycloud-platform-main/src/components/ui/table.tsx
+++ greycloud-platform-main/src/components/ui/table.tsx
@@ -72,8 +72,9 @@ const TableHead = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <th
     ref={ref}
+    style={{color:"white"}}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 text-left align-middle font-medium [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
